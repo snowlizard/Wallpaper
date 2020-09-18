c37e22406834bc05243cfb3d2f53d996162ce3df
snowlizard <illegal_burrito@live.com>
2020-09-18 16:10:18 -0700

    finish rest later

--- src/c/app.js
+++ src/c/app.js
@@ -1,9 +1,19 @@
 const App = document.getElementById('app')
 
+const image_list = ['../images/20171029_154455.jpg',
+    '../images/20171029_155224.jpg',
+    '../images/20171029_155346.jpg',
+    '../images/20171029_160212.jpg',
+    '../images/20190603_193934.jpg',
+    '../images/20190604_120306.jpg',
+    '../images/crew_onRoad.jpg',
+    '../images/IMG_0968.jpg'];
+
 // crate an image tag
-const createTile = (name, id) => {
+const createTile = (source, id) => {
     let tag = document.createElement('img')
-    tag.setAttribute('src','../images/'+ name)
+    tag.setAttribute('src', name)
+    tag.className += 'tiles'
     tag.id = id
     App.appendChild(tag)
 }
@@ -11,7 +21,7 @@ const createTile = (name, id) => {
 const screen_size = screen.width;
 
 if( screen_size <= 1920 ){
-    
+
 }else{
 
 }
--- src/c/getNames.py
+++ src/c/getNames.py
@@ -10,7 +10,10 @@ print('I\'m on it boss!')
 
 textDump = open('./src/c/image_names.txt', "w+")
 
-for name in onlyfiles:
-    textDump.write(name + '\n')
+textDump.write('[')
 
+for name in onlyfiles:
+    textDump.write('\'' + '../images/' + name + '\',' +'\n')
+
+textDump.write(']')
 textDump.close()
